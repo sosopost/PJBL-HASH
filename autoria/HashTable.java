d9075cffe85a15c9c7ef7e77fe0caf7bc20c2192
Sophia Post Ploposki <sophiapost.ploposki@icloud.com>
2025-10-15 20:01:35 -0300

    Atalizado
    Removed commented-out methods for detailed statistics.

--- autoria/HashTable.java
+++ autoria/HashTable.java
@@ -8,7 +8,6 @@ public interface HashTable {
     void estatisticas();
     double getFatorCarga();
     
-    // Métodos para extrair estatísticas detalhadas
     default int getMaiorLista() { return 0; }
     default List<Integer> getTresMaioresListas() { return new ArrayList<>(); }
     default int[] getGaps() { return new int[]{0, 0, 0}; }
